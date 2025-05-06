275f15c4d08b735d3bf8424bfe8679e82b1cfef0
Dan Ni <61080530+Danni4real@users.noreply.github.com>
2025-05-06 15:28:44 +0800

    Create example.cpp

---
+++ example.cpp
@@ -0,0 +1,69 @@
+#include <thread>
+#include <iostream>
+
+#include "Socket.h"
+
+Socket socket_server;
+
+void wait_client_message(int client_fd) {
+    while (true) {
+        std::string str = socket_server.recv(client_fd);
+        if (str.empty()) {
+            std::cout << "recv() failed!" << std::endl;
+            return;
+        }
+
+        std::cout << "server: recvd a message: " << str << std::endl;
+
+        socket_server.send(client_fd, "ack: " + str);
+    }
+}
+
+void create_client_loop(Socket &client, const std::string &client_name) {
+    std::thread([=,&client]() {
+        client.run_as_client("127.0.0.1", 8088);
+
+        int i = 0;
+        std::thread([&] {
+            while (true) {
+                std::string str = client.recv();
+                if (str.empty()) {
+                    std::cout << "recv() failed!" << std::endl;
+                    return;
+                }
+                std::cout << client_name << ": recvd a message: " << str << std::endl;
+
+                std::this_thread::sleep_for(std::chrono::milliseconds(1));
+                client.send(client_name + ": " + std::to_string(++i));
+            }
+        }).detach();
+
+        client.send(client_name + ": " + std::to_string(i));
+
+        while (true) {
+            std::this_thread::sleep_for(std::chrono::seconds(60));
+        }
+    }).detach();
+}
+
+int main() {
+    socket_server.run_as_server(8088,
+                                2,
+                                [](int client_fd) {
+                                    std::thread([=] {
+                                        wait_client_message(client_fd);
+                                    }).detach();
+                                });
+
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+
+    Socket socket_client1;
+    Socket socket_client2;
+
+    create_client_loop(socket_client1, "client1");
+    create_client_loop(socket_client2, "client2");
+
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::seconds(60));
+    }
+}
