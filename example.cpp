2abd834663331affa80b917b76813ff5225c2c24
Dan Ni <61080530+Danni4real@users.noreply.github.com>
2025-05-09 15:49:13 +0800

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -3,26 +3,41 @@
 
 #include "Socket.h"
 
-Socket socket_server;
+Socket tcp_server;
+Socket udp_server;
 
-void handle_new_client_connection(int client_fd) {
+void handle_tcp_client_connected(int client_fd) {
     std::thread([=]() {
         while (true) {
-            std::string str = socket_server.recv(client_fd);
+            std::string str = tcp_server.recv(client_fd);
             if (str.empty()) {
                 std::cout << "recv() failed!" << std::endl;
-                return;
+                exit(1);
             }
 
-            std::cout << "server: recvd a message: " << str << std::endl;
-            socket_server.send(client_fd, "ack: " + str);
+            std::cout << "tcp server recvd: " << str << std::endl;
+            tcp_server.send(client_fd, "ack--" + str);
         }
     }).detach();
 }
 
-void run_client(Socket &client, const std::string &client_name) {
+void wait_udp_messages() {
+    std::thread([]() {
+        while (true) {
+            std::string str = udp_server.recv();
+            if (str.empty()) {
+                std::cout << "recv() failed!" << std::endl;
+                exit(1);
+            }
+
+            std::cout << "udp server recvd: " << str << std::endl;
+        }
+    }).detach();
+}
+
+void run_tcp_client(Socket &client, const std::string &client_name) {
     std::thread([=,&client]() {
-        client.run_as_client("127.0.0.1", 8088);
+        client.run_as_tcp_client("127.0.0.1", 8088);
 
         int i = 0;
         std::thread([&] {
@@ -30,16 +45,16 @@ void run_client(Socket &client, const std::string &client_name) {
                 std::string str = client.recv();
                 if (str.empty()) {
                     std::cout << "recv() failed!" << std::endl;
-                    return;
+                    exit(1);
                 }
-                std::cout << client_name << ": recvd a message: " << str << std::endl;
+                std::cout << client_name << " recvd: " << str << std::endl;
 
-                std::this_thread::sleep_for(std::chrono::milliseconds(1));
-                client.send(client_name + ": " + std::to_string(++i));
+                std::this_thread::sleep_for(std::chrono::microseconds(1));
+                client.send(client_name + "--" + std::to_string(++i));
             }
         }).detach();
 
-        client.send(client_name + ": " + std::to_string(i));
+        client.send(client_name + "--" + std::to_string(i));
 
         while (true) {
             std::this_thread::sleep_for(std::chrono::seconds(60));
@@ -47,18 +62,46 @@ void run_client(Socket &client, const std::string &client_name) {
     }).detach();
 }
 
+void run_udp_client(Socket &client, const std::string &client_name) {
+    std::thread([=,&client]() {
+        client.run_as_udp_client();
+
+        int i = 0;
+        while (true) {
+            bool ret = client.send("127.0.0.1",
+                                   8087,
+                                   client_name + "--" + std::to_string(++i));
+            if (!ret) {
+                std::cout << "send() failed!" << std::endl;
+                exit(1);
+            }
+
+            std::this_thread::sleep_for(std::chrono::microseconds(1));
+        }
+    }).detach();
+}
+
 int main() {
-    socket_server.run_as_server(8088,
-                                2,
-                                handle_new_client_connection);
+    udp_server.run_as_udp_server(8087);
+    wait_udp_messages();
+
+    tcp_server.run_as_tcp_server(8088,
+                                 2,
+                                 handle_tcp_client_connected);
 
     std::this_thread::sleep_for(std::chrono::seconds(3));
 
-    Socket socket_client1;
-    Socket socket_client2;
+    Socket tcp_client1;
+    Socket tcp_client2;
 
-    run_client(socket_client1, "client1");
-    run_client(socket_client2, "client2");
+    Socket udp_client1;
+    Socket udp_client2;
+
+    run_tcp_client(tcp_client1, "tcp_client1");
+    run_tcp_client(tcp_client2, "tcp_client2");
+
+    run_udp_client(udp_client1, "udp_client1");
+    run_udp_client(udp_client2, "udp_client2");
 
     while (true) {
         std::this_thread::sleep_for(std::chrono::seconds(60));
