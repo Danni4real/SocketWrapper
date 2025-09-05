db7e2fa40d04e61685cb4b2ab54f98bee720f250
Dan Ni <61080530+Danni4real@users.noreply.github.com>
2025-09-05 16:26:51 +0800

    Update Socket.cpp

--- Socket.cpp
+++ Socket.cpp
@@ -12,22 +12,18 @@
 #include <iostream>
 
 #include "Socket.h"
-#include "ThreadLog.h"
 
-bool Socket::run_as_server(const uint port,
-                           const uint client_max_num,
-                           const std::function<void(int)> &new_client_connect_callback) {
-    LOG_CALL(port, client_max_num);
+bool Socket::run_as_udp_server(uint port) {
     std::lock_guard lk(m_current_role_mtx);
 
     if (m_current_role != NOT_SET) {
-        LOG_ERROR("Role already set!");
+        std::cout << "Err: Role already set!" << std::endl;
         return false;
     }
 
-    m_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    m_socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
     if (m_socket_fd == -1) {
-        LOG_ERROR("socket() failed!");
+        std::cout << "Err: socket() failed!" << std::endl;
         return false;
     }
 
@@ -36,24 +32,56 @@ bool Socket::run_as_server(const uint port,
     addr.sin_addr.s_addr = INADDR_ANY;
     addr.sin_port = htons(port);
     if (bind(m_socket_fd, (sockaddr *) &addr, sizeof(addr)) < 0) {
-        LOG_ERROR("bind() failed!");
+        std::cout << "Err: bind() failed!" << std::endl;
+
+        close();
+        return false;
+    }
+
+    m_current_role = UDP_SERVER;
+
+    return true;
+}
+
+bool Socket::run_as_tcp_server(const uint port,
+                               const uint client_max_num,
+                               const std::function<void(int)> &new_client_connect_callback) {
+    std::lock_guard lk(m_current_role_mtx);
+
+    if (m_current_role != NOT_SET) {
+        std::cout << "Err: Role already set!" << std::endl;
+        return false;
+    }
+
+    m_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (m_socket_fd == -1) {
+        std::cout << "Err: socket() failed!" << std::endl;
+        return false;
+    }
+
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(port);
+    if (bind(m_socket_fd, (sockaddr *) &addr, sizeof(addr)) < 0) {
+        std::cout << "Err: bind() failed!" << std::endl;
 
         close();
         return false;
     }
 
     if (listen(m_socket_fd, client_max_num) < 0) {
-        LOG_ERROR("listen() failed!");
+        std::cout << "Err: listen() failed!" << std::endl;
 
         close();
         return false;
     }
 
     std::thread([=] {
-        while (true) {
+        for (int i = 0; i < client_max_num; i++) {
             int client_fd = accept(m_socket_fd, nullptr, nullptr);
             if (client_fd < 0) {
-                LOG_ERROR("accept() failed!");
+                std::cout << "Err: accept() failed!" << std::endl;
                 continue;
             }
 
@@ -66,22 +94,41 @@ bool Socket::run_as_server(const uint port,
         }
     }).detach();
 
-    m_current_role = SERVER;
+    m_current_role = TCP_SERVER;
+
     return true;
 }
 
-bool Socket::run_as_client(const std::string &ip, const uint port) {
-    LOG_CALL(ip, port);
+bool Socket::run_as_udp_client() {
     std::lock_guard lk(m_current_role_mtx);
 
     if (m_current_role != NOT_SET) {
-        LOG_ERROR("Role already set!");
+        std::cout << "Err: Role already set!" << std::endl;
+        return false;
+    }
+
+    m_socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (m_socket_fd == -1) {
+        std::cout << "Err: socket() failed!" << std::endl;
+        return false;
+    }
+
+    m_current_role = UDP_CLIENT;
+
+    return true;
+}
+
+bool Socket::run_as_tcp_client(const std::string &ip, const uint port) {
+    std::lock_guard lk(m_current_role_mtx);
+
+    if (m_current_role != NOT_SET) {
+        std::cout << "Err: Role already set!" << std::endl;
         return false;
     }
 
     m_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (m_socket_fd == -1) {
-        LOG_ERROR("socket() failed!");
+        std::cout << "Err: socket() failed!" << std::endl;
         return false;
     }
 
@@ -90,12 +137,13 @@ bool Socket::run_as_client(const std::string &ip, const uint port) {
     server_addr.sin_port = htons(port);
     inet_pton(AF_INET, ip.c_str(), &server_addr.sin_addr);
     if (connect(m_socket_fd, (sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
-        LOG_ERROR("connect() failed!");
+        std::cout << "Err: connect() failed!" << std::endl;
+
         close();
         return false;
     }
 
-    m_current_role = CLIENT;
+    m_current_role = TCP_CLIENT;
 
     m_fd_to_send_mutex_map[m_socket_fd];
     m_fd_to_recv_mutex_map[m_socket_fd];
@@ -106,76 +154,88 @@ bool Socket::run_as_client(const std::string &ip, const uint port) {
 }
 
 bool Socket::send(const std::string &str) {
-    return send(m_socket_fd, str);
+    if (m_current_role != TCP_CLIENT) {
+        std::cout << "Err: wrong send api called!" << std::endl;
+        return false;
+    }
+
+    return tcp_send(m_socket_fd, str);
 }
 
-bool Socket::send(int socket_fd, const std::string &str) {
-    if (m_current_role == NOT_SET) {
-        LOG_ERROR("Role not set yet!");
+bool Socket::send(int client_fd, const std::string &str) {
+    if (m_current_role != TCP_SERVER) {
+        std::cout << "Err: wrong send api called!" << std::endl;
         return false;
     }
 
-    if (socket_fd == INVALID_SOCKET_FD) {
-        LOG_ERROR("invalid socket fd!");
+    return tcp_send(client_fd, str);
+}
+
+bool Socket::send(const std::string &ip, uint port, const std::string &str) {
+    if (m_current_role != UDP_CLIENT) {
+        std::cout << "Err: wrong send api called!" << std::endl;
         return false;
     }
 
-    if (str.length() > MSG_MAX_LEN) {
-        LOG_ERROR("string oversized!");
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    inet_pton(AF_INET, ip.c_str(), &addr.sin_addr);
+
+    auto str_len = str.length();
+    int ret = sendto(m_socket_fd, str.data(), str_len, 0, (sockaddr *) &addr, sizeof(addr));
+    if (ret < str_len) {
+        std::cout << "Err: sendto() failed!" << std::endl;
         return false;
     }
 
-    return inner_send(socket_fd, str);
+    return true;
 }
 
 std::string Socket::recv() {
-    return recv(m_socket_fd);
+    if (m_current_role == TCP_CLIENT)
+        return tcp_recv(m_socket_fd);
+    if (m_current_role == UDP_SERVER)
+        return udp_recv();
+
+    std::cout << "Err: wrong recv api called!" << std::endl;
+    return "";
 }
 
 std::string Socket::recv(int socket_fd) {
-    if (m_current_role == NOT_SET) {
-        LOG_ERROR("Role not set yet!");
+    if (m_current_role != TCP_SERVER) {
+        std::cout << "Err: wrong recv api called!" << std::endl;
         return "";
     }
 
-    if (socket_fd == INVALID_SOCKET_FD) {
-        LOG_ERROR("invalid socket fd!");
-        return "";
-    }
-
-    return inner_recv(socket_fd);
+    return tcp_recv(socket_fd);
 }
 
 void Socket::close() {
-    LOG_CALL_0();
-
     if (m_socket_fd != INVALID_SOCKET_FD) {
         ::close(m_socket_fd);
-
-        m_fd_to_send_mutex_map.erase(m_socket_fd);
-        m_fd_to_recv_mutex_map.erase(m_socket_fd);
-        m_fd_to_send_mem_map.erase(m_socket_fd);
-        m_fd_to_recv_mem_map.erase(m_socket_fd);
-
         m_socket_fd = INVALID_SOCKET_FD;
     }
 }
 
 void Socket::close(int client_fd) {
-    LOG_CALL(client_fd);
-
     if (client_fd != INVALID_SOCKET_FD) {
         ::close(client_fd);
-
-        m_fd_to_send_mutex_map.erase(client_fd);
-        m_fd_to_recv_mutex_map.erase(client_fd);
-        m_fd_to_send_mem_map.erase(client_fd);
-        m_fd_to_recv_mem_map.erase(client_fd);
     }
 }
 
-bool Socket::inner_send(int fd, const std::string &str) {
+bool Socket::tcp_send(int fd, const std::string &str) {
     std::lock_guard lk(m_fd_to_send_mutex_map[fd]);
+
+    if (fd == INVALID_SOCKET_FD) {
+        std::cout << "Err: invalid socket fd!" << std::endl;
+        return false;
+    }
+
+    if (str.length() > MSG_MAX_LEN) {
+        std::cout << "Err: string oversized!" << std::endl;
+        return false;
+    }
 
     char *buf = m_fd_to_send_mem_map[fd].get();
 
@@ -189,7 +249,7 @@ bool Socket::inner_send(int fd, const std::string &str) {
     while (bytes_sent < msg_len) {
         int ret = ::send(fd, buf + bytes_sent, msg_len - bytes_sent, 0);
         if (ret <= 0) {
-            LOG_ERROR("send failed, socket may be disconnected!");
+            std::cout << "Err: send failed, socket may be disconnected!" << std::endl;
             return false;
         }
         bytes_sent += ret;
@@ -198,14 +258,19 @@ bool Socket::inner_send(int fd, const std::string &str) {
     return true;
 }
 
-std::string Socket::inner_recv(int fd) {
+std::string Socket::tcp_recv(int fd) {
     std::lock_guard lk(m_fd_to_recv_mutex_map[fd]);
+
+    if (fd == INVALID_SOCKET_FD) {
+        std::cout << "Err: invalid socket fd!" << std::endl;
+        return "";
+    }
 
     char *buf = m_fd_to_recv_mem_map[fd].get();
 
     int ret = ::recv(fd, buf, 4, 0);
     if (ret <= 0) {
-        LOG_ERROR("recv failed, socket may be disconnect!");
+        std::cout << "Err: recv failed, socket may be disconnect!" << std::endl;
         return "";
     }
 
@@ -214,7 +279,7 @@ std::string Socket::inner_recv(int fd) {
 
     if (msg_len > MSG_MAX_LEN) {
         // normally, this may never happen
-        LOG_ERROR("message oversized!");
+        std::cout << "Fatal: message oversized!" << std::endl;
         exit(1);
     }
 
@@ -222,10 +287,27 @@ std::string Socket::inner_recv(int fd) {
     while (bytes_recvd < msg_len) {
         int ret = ::recv(fd, buf + bytes_recvd, msg_len - bytes_recvd, 0);
         if (ret <= 0) {
-            LOG_ERROR("recv failed, socket may be disconnect!");
+            std::cout << "Err: recv failed, socket may be disconnect!" << std::endl;
             return "";
         }
         bytes_recvd += ret;
+    }
+
+    return std::string(buf, msg_len);
+}
+
+std::string Socket::udp_recv() {
+    static std::mutex mtx;
+    std::lock_guard lk(mtx);
+
+    static char buf[MSG_MAX_LEN] = {};
+    static sockaddr addr{};
+    static socklen_t addr_len = sizeof(addr);
+
+    int msg_len = recvfrom(m_socket_fd, buf, MSG_MAX_LEN, 0, &addr, &addr_len);
+    if (msg_len <= 0) {
+        std::cout << "Err: recvfrom() failed!" << std::endl;
+        return "";
     }
 
     return std::string(buf, msg_len);
